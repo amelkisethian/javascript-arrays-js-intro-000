ed27c475fb5e410a081fc7326fb538e9df4c4803
amelkisethian <angela.melkisethian@gmail.com>
2017-11-17 04:37:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,14 +6,7 @@ var chocolateBars = [
 ]
 
 function addElementToBeginningOfArray(element,array) {
-  var array = [2,3]
-  [1, ...array]
-  return array
 }
 
 function destructivelyAddElementToBeginningOfArraya (element,array) {
-
-var array = ["New York", "San Francisco"]
- 
-array.unshift("element")
 }
